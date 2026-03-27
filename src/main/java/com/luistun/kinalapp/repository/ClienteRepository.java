ff4c61166252f4759758aa647bda92fbed6ecfc5
Lsoc-2025576 <Lsoc-2025576@kinal.edu.gt>
2026-03-26 21:37:34 -0600

    feat: se agrego la interface de usuarios

--- src/main/java/com/luistun/kinalapp/repository/ClienteRepository.java
+++ src/main/java/com/luistun/kinalapp/repository/ClienteRepository.java
@@ -2,7 +2,6 @@ package com.luistun.kinalapp.repository;
 
 import com.luistun.kinalapp.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente,String> {
 
